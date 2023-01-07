023c26171675f85342f9aa22078609c747165c79
IvanTuly <ivantuly@gmail.com>
2023-01-07 20:14:52 +0300

    text color variable from color scheme

--- js/settings.js
+++ js/settings.js
@@ -1,9 +1,12 @@
 const navBar = document.querySelector("nav"),
-menuBtns = document.querySelectorAll(".mainmenu-icon"),
-overlay = document.querySelector(".overlay");
-let colorScheme = "red"
+    menuBtns = document.querySelectorAll(".mainmenu-icon"),
+    overlay = document.querySelector(".overlay");
+let colorScheme = "red";
+let colorText = "white";
+
 var settings = {
-    colorScheme: colorScheme
+    colorScheme: colorScheme,
+    colorText: colorText
 }
 
 if (localStorage.getItem('settings')) {
@@ -13,56 +16,71 @@ if (localStorage.getItem('settings')) {
 
 console.log(settings)
 document.documentElement.style.setProperty('--main-color-scheme', settings.colorScheme);
+document.documentElement.style.setProperty('--main-text-scheme', settings.colorText);
 
 //открытие и закрытие меню
 menuBtns.forEach((menuBtn) => {
-menuBtn.addEventListener("click", () => {
-  navBar.classList.toggle("open");
-});
+    menuBtn.addEventListener("click", () => {
+        navBar.classList.toggle("open");
+    });
 });
 overlay.addEventListener("click", () => {
-navBar.classList.remove("open");
+    navBar.classList.remove("open");
 });
-document.querySelector(".openmenu-icon").addEventListener("click", ()=>{
+document.querySelector(".openmenu-icon").addEventListener("click", () => {
     navBar.classList.remove("open");
 })
 
 
 //переключение цвета по кнопкам
-document.querySelector(".colorButton__colorRed").addEventListener("click", ()=>{
-document.documentElement.style.setProperty('--main-color-scheme', 'red');
-settings.colorScheme = "red"
-saveToLocalStorage()
-})
-document.querySelector(".colorButton__colorBlack").addEventListener("click", ()=>{
-    document.documentElement.style.setProperty('--main-color-scheme', 'black');
-    settings.colorScheme = "black"
+document.querySelector(".colorButton__colorRed").addEventListener("click", () => {
+    document.documentElement.style.setProperty('--main-color-scheme', 'red');
+    document.documentElement.style.setProperty('--main-text-scheme', 'white');
+    settings.colorScheme = "red"
+    settings.colorText = "white"
     saveToLocalStorage()
 })
-document.querySelector(".colorButton__colorGreen").addEventListener("click", ()=>{
-document.documentElement.style.setProperty('--main-color-scheme', 'green');
-settings.colorScheme = "green"
-saveToLocalStorage()
+document.querySelector(".colorButton__colorBlack").addEventListener("click", () => {
+    document.documentElement.style.setProperty('--main-color-scheme', 'black');
+    document.documentElement.style.setProperty('--main-text-scheme', 'white');
+    settings.colorScheme = "black"
+    settings.colorText = "white"
+    saveToLocalStorage()
 })
-document.querySelector(".colorButton__colorBlue").addEventListener("click", ()=>{
-document.documentElement.style.setProperty('--main-color-scheme', 'blue');
-settings.colorScheme = "blue"
-saveToLocalStorage()
+document.querySelector(".colorButton__colorGreen").addEventListener("click", () => {
+    document.documentElement.style.setProperty('--main-color-scheme', 'green');
+    document.documentElement.style.setProperty('--main-text-scheme', 'white');
+    settings.colorScheme = "green"
+    settings.colorText = "white"
+    saveToLocalStorage()
 })
-document.querySelector(".colorButton__colorYellow").addEventListener("click", ()=>{
-document.documentElement.style.setProperty('--main-color-scheme', 'yellow');
-settings.colorScheme = "yellow"
-saveToLocalStorage()
+document.querySelector(".colorButton__colorBlue").addEventListener("click", () => {
+    document.documentElement.style.setProperty('--main-color-scheme', 'blue');
+    document.documentElement.style.setProperty('--main-text-scheme', 'white');
+    settings.colorScheme = "blue"
+    settings.colorText = "white"
+    saveToLocalStorage()
 })
-document.querySelector(".colorButton__colorPink").addEventListener("click", ()=>{
-document.documentElement.style.setProperty('--main-color-scheme', 'pink');
-settings.colorScheme = "pink"
-saveToLocalStorage()
+document.querySelector(".colorButton__colorYellow").addEventListener("click", () => {
+    document.documentElement.style.setProperty('--main-color-scheme', 'yellow');
+    document.documentElement.style.setProperty('--main-text-scheme', 'black');
+    settings.colorScheme = "yellow"
+    settings.colorText = "black"
+    saveToLocalStorage()
 })
-document.querySelector(".colorButton__colorPurple").addEventListener("click", ()=>{
-document.documentElement.style.setProperty('--main-color-scheme', 'purple');
-settings.colorScheme = "purple"
-saveToLocalStorage()
+document.querySelector(".colorButton__colorPink").addEventListener("click", () => {
+    document.documentElement.style.setProperty('--main-color-scheme', 'pink');
+    document.documentElement.style.setProperty('--main-text-scheme', 'white');
+    settings.colorScheme = "pink"
+    settings.colorText = "white";
+    saveToLocalStorage()
+})
+document.querySelector(".colorButton__colorPurple").addEventListener("click", () => {
+    document.documentElement.style.setProperty('--main-color-scheme', 'purple');
+    document.documentElement.style.setProperty('--main-text-scheme', 'white');
+    settings.colorScheme = "purple"
+    settings.colorText = "white";
+    saveToLocalStorage()
 })
 
 
